ccdade7004eef113394b7d87ab4f917aeb85fb61
JayJay <58577865+J88Y@users.noreply.github.com>
2022-03-15 15:40:27 +0900

    commit

--- app/src/main/java/com/example/uncaught_exception_handle/activity/common/BaseActivity.kt
+++ app/src/main/java/com/example/uncaught_exception_handle/activity/common/BaseActivity.kt
@@ -4,30 +4,21 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.uncaught_exception_handle.activity.ErrorActivity
-import com.orhanobut.logger.Logger
 import kotlin.system.exitProcess
 
 open class BaseActivity : AppCompatActivity() {
-    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         
-        onUncaughtException()
-        // Process.killProcess(Process.myPid())
-        // exitProcess(-1)
-    }
-    
-    private fun onUncaughtException(/*onException: () -> Unit*/) {
-        // 예상치 못한 에러 발생
+        // RunTimeException 발생시 동작
         Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
-            Logger.d("thread: $thread / throwable: $throwable")
-            
+            // 에러 액티비티로 이동.
             val intent = Intent(this, ErrorActivity::class.java)
             startActivity(intent)
             
-            // Process.killProcess(Process.myPid())
-            exitProcess(-1)
+            // 현재 프로세스를 나가야 함.
+            // 아래 메서드를 실행하지 않으면 Thread 안에 갇히게 됨.
+            exitProcess(0) // == System.exit(status)
         }
     }
-    
 }
